f64fb467a714846dc486522f03eb61f35da161a2
makroterno <kagan.akar1@gmail.com>
2024-06-09 13:21:14 +0300

    add basic pong behaviour, paddle - wall relationship, paddle ball relationship

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 var X_SPEED : float = 100.0
-var Y_SPEED : float = 0.0
+var Y_SPEED : float = 100.0
 var velocity_vector = Vector2(X_SPEED, Y_SPEED)
-var direction = 1
+var direction = Vector2(1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +12,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocity_vector * direction * delta
+	position += Vector2(velocity_vector.x * direction.x , velocity_vector.y * direction.y) * delta
+	
+# Add top, bottom and wall collisions
+
+func _on_area_entered(area):
+	if area.name == "Top" or area.name == "Bottom":
+		direction.y = -direction.y
+	
+	elif area.name == "Left" or area.name == "Right":
+		get_tree().reload_current_scene()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,37 @@
 extends Node2D
 class_name Player
-var velocity : float = 100.0
+var velocity : float = 500.0
 @export var sprite : Texture2D
-@export var direction : int = 1
+
+@export var ball_direction = Vector2(1, 0)
+var _ball_direction = Vector2(1, 0)
+var paddle_direction : int = 1
+
 @export var control_up : String = ""
 @export var control_down : String = ""
-var y_size = get_viewport_rect().size # May be buggy if there will be a size change since this only initalizes once
+
+@onready var y_size = get_viewport_rect().size.y # May be buggy if there will be a size change since this only initalizes once
+@onready var sprite_2d = $Sprite2D
+
+
+
+func _ready():
+	sprite_2d.texture = sprite
+	_ball_direction = ball_direction
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	direction = Input.get_axis(control_down, control_up)
-	if direction:
-		position.y += velocity * direction
+	paddle_direction = Input.get_axis(control_up, control_down)
+	if paddle_direction:
+		position.y += velocity * paddle_direction * delta
+		position.y = clamp(position.y, 60, y_size - 60)
 		
-func _on_body_entered(body):
-	body.direction = direction
+func _on_area_entered(area):
+	if area.name == "Ball":
+		var rng = RandomNumberGenerator.new()
+		print("rng: ", rng)
+		print("Ball entered the paddle")
+		#TODO: Increase this code quality & improve ball behaviour
+		area.direction = Vector2(_ball_direction.x, rng.randf_range(-1.0, 1.0))
+
+
